45953e62564b933619e019273bb5d643228bc323
dasWarder <Al.94_94@mail.ru>
2021-11-15 14:55:34 +0300

    Create Purchases consumer module to store information about each purchase GT 5$

---
+++ PurchasesConsumer/src/main/java/com/example/zmart/purchasesconsumer/PurchasesConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.zmart.purchasesconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchasesConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PurchasesConsumerApplication.class, args);
+  }
+}
---
+++ PurchasesConsumer/src/test/java/com/example/zmart/purchasesconsumer/PurchasesConsumerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.zmart.purchasesconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesConsumerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
